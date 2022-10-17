41e3c07a1e4a214361497a10d7832150df6565eb
nicolekarakin <nicole.karakin@gmail.com>
2022-10-17 12:57:36 +0200

    first commit

---
+++ src/main/java/org/nnn4eu/shopaddressesapi/ShopaddressesApiApplication.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.shopaddressesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopaddressesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopaddressesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nnn4eu/shopaddressesapi/ShopaddressesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.shopaddressesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopaddressesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
